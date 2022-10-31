57fa945013f2cc09e5d2af4ccb985939b5c82337
Kevin <dragon0170@gmail.com>
2022-10-31 22:18:54 +0900

    Fix pintos_setup script for working other projects
    threads에서 build된 pintos가 아닌 다른 디렉토리의 pintos도 사용하도록
    pintos_setup script를 수정.
    project 2에서는 userprog에서 빌드된 pintos를 사용해야 file system
    format이 가능함.

--- pintos_setup.sh
+++ pintos_setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "You should specify the directory for the project. (e.g. userprog)"
+  return 1
+fi
+
 dirname=${PWD}
 echo $dirname
-sed -i "257 c\        my \$name = find_file ('$dirname/src/threads/build/kernel.bin');" ./src/utils/pintos
-sed -i "362 c\    \$name = find_file ('$dirname/src/threads/build/loader.bin') if !defined \$name;" ./src/utils/Pintos.pm
+sed -i "257 c\        my \$name = find_file ('$dirname/src/$1/build/kernel.bin');" ./src/utils/pintos
+sed -i "362 c\    \$name = find_file ('$dirname/src/$1/build/loader.bin') if !defined \$name;" ./src/utils/Pintos.pm
 echo "export PATH=$dirname/src/utils:\$PATH" >> ~/.bashrc
 source ~/.bashrc
